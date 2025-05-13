e51d9d8263c18df970739e78309b74470883e4d4
Andrei Stoica <68513477+andreicstoica@users.noreply.github.com>
2025-05-13 10:29:14 -0400

    Zod request validation implemented to POST endpoint

--- part9/patientor-backend/src/routes/patient.ts
+++ part9/patientor-backend/src/routes/patient.ts
@@ -1,8 +1,8 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import patientService from '../services/patientService';
+import { NewPatientSchema } from '../utils';
 import { Patient, NewPatient } from '../types';
-import { Response } from 'express';
-import toNewPatient from '../utils';
+import z from 'zod';
 
 const router = express.Router();
 
@@ -10,12 +10,28 @@ router.get('/', (_req, res: Response<Patient[]>) => {
     res.send(patientService.getPatients());
 });
 
-router.post('/', (req, res) => {
-    const newPatientEntry = toNewPatient(req.body);
+const newPatientParser = (req: Request, _res: Response, next: NextFunction) => { 
+  try {
+    NewPatientSchema.parse(req.body);
+    next();
+  } catch (error: unknown) {
+    next(error);
+  }
+};
 
-    const addedPatientEntry: NewPatient = patientService.addPatient(newPatientEntry);
+const errorMiddleware = (error: unknown, _req: Request, res: Response, next: NextFunction) => { 
+  if (error instanceof z.ZodError) {
+    res.status(400).send({ error: error.issues });
+  } else {
+    next(error);
+  }
+};
 
-    res.json(addedPatientEntry);
+router.post('/', newPatientParser, (req: Request<unknown, unknown, NewPatient>, res: Response<Patient>) => {
+    const addedPatient = patientService.addPatient(req.body);
+    res.json(addedPatient);
 });
+
+router.use(errorMiddleware);
 
 export default router;
--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -1,22 +1,25 @@
-export type Diagnosis = {
-    code: string,
-    name: string,
-    latin?: string
-};
-
-export type Patient = {
-    id: string,
-    name: string,
-    dateOfBirth: string,
-    ssn: string,
-    gender: string,
-    occupation: string
-};
-
-export type NewPatient = Omit<Patient, 'id'>;
+import { z } from 'zod';
+import { NewPatientSchema } from './utils';
 
 export enum Gender {
     Male = "male",
     Female = "female",
     Other = "other"
 };
+
+export interface Diagnosis {
+    code: string,
+    name: string,
+    latin?: string
+};
+
+export interface Patient {
+    id: string;
+    gender: string;
+    name: string;
+    ssn: string;
+    occupation: string;
+    dateOfBirth: string;
+};
+
+export type NewPatient = z.infer<typeof NewPatientSchema>;
